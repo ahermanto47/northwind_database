b7fa1df4e0a83ce3dd78694e7c42498b10cf75b7
Andi Hermanto <ahermanto47@gmail.com>
2024-12-01 12:02:32 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -16,14 +16,42 @@ get_object_info_from_filename() {
 
 }
 
-call_sqlcmd() {
+call_sqlcmd_v2() {
 
-    result=$(sqlcmd -W -w 999 -h-1 -s"," -i input.sql)
-    echo "$result"
+    result=$(sqlcmd -u -W -w 999 -h-1 -s"," -i input.sql | tr -d '\r')
+    echo "$result" > file1
 
 }
 
-get_table_info_from_db() {
+# get_table_info_from_db() {
+
+#     cat > input.sql << EOM
+# :setvar SQLCMDERRORLEVEL 1
+# USE DEV
+# GO
+# SET NOCOUNT ON
+# GO
+# SELECT 
+#    CONCAT(
+#    '[' , c.name, '] ',
+#    '[', p.name, ']' ,
+#    case when p.name in ('nvarchar') then CONCAT('(' , p.precision, ') ') else ' ' end,
+#    case when c.is_identity = 1 then 'IDENTITY(1,1) ' else '' end,
+#    case when c.is_nullable = 1 then 'NULL,' else 'NOT NULL,' end 
+#    )
+# FROM sys.columns AS c
+# INNER JOIN sys.tables AS t ON t.object_id = c.object_id
+# INNER JOIN sys.schemas AS s ON s.schema_id = t.schema_id
+# INNER JOIN sys.types AS p ON p.user_type_id = c.user_type_id
+# WHERE s.name = '$1' and t.name = '$2'
+# GO
+# EOM
+
+#     call_sqlcmd
+
+# }
+
+get_table_info_from_db_v3() {
 
     cat > input.sql << EOM
 :setvar SQLCMDERRORLEVEL 1
@@ -31,23 +59,82 @@ USE DEV
 GO
 SET NOCOUNT ON
 GO
-SELECT 
-   s.name as schema_name, 
-   t.name as table_name, 
-   c.name as column_name 
-FROM sys.columns AS c
-INNER JOIN sys.tables AS t ON t.object_id = c.object_id
-INNER JOIN sys.schemas AS s ON s.schema_id = t.schema_id
-WHERE s.name = '$1' and t.name = '$2'
+select 
+   CONCAT(
+   '[' , COLUMN_NAME, '] ',
+   '[', DATA_TYPE, ']' ,
+   case when DATA_TYPE in ('nvarchar','nchar') then CONCAT('(' , CHARACTER_MAXIMUM_LENGTH, ') ') else ' ' end,
+   case when exists ( 
+        select id from syscolumns
+        where object_name(id)= '$2'
+        and name=column_name
+        and columnproperty(id,name,'IsIdentity') = 1 
+   ) then
+        'IDENTITY(' + 
+        cast(ident_seed('$2') as varchar) + ',' + 
+        cast(ident_incr('$2') as varchar) + ') '
+   else '' end,
+   case when IS_NULLABLE = 'YES' then 'NULL,' else 'NOT NULL,' end 
+   )
+from INFORMATION_SCHEMA.columns 
+where TABLE_SCHEMA = '$1' AND TABLE_NAME = '$2'
 GO
 EOM
 
-    call_sqlcmd
+    call_sqlcmd_v2
 
 }
 
-# param can be full or delta
-echo "Received param $1"
+get_table_info_from_file() {
+
+    result=$(sed -e '/CREATE/,/GO/!d' "$1" | grep -e ',' | grep -v 'ASC,' | grep -v ')WITH' | sed -e 's/^\s*//')
+    echo "$result"
+
+}
+
+get_table_info_from_file_v2() {
+
+    result=$(sed -e '/CREATE/,/GO/!d' "$1" | grep -e ',' | grep -v 'ASC,' | grep -v ')WITH' | sed -e 's/^\s*//')
+    echo "$result" > file2
+
+}
+
+find_difference() {
+
+    result=$(diff -b <(echo "$1") <(echo "$2"))
+    echo "$result"
+
+}
+
+find_difference_v2() {
+
+    result=$(diff file1 file2)
+    echo "$result"
+
+}
+
+# mode can be full or delta
+# debug can be 1 or 0
+while getopts ":d:m:" opt; do
+  case $opt in
+    d) debug="$OPTARG"
+    ;;
+    m) mode="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+printf "Argument debug is %s\n" "$debug"
+printf "Argument mode is %s\n" "$mode"
 
 mkdir build
 cp dbo.*.sql build
@@ -55,7 +142,7 @@ cd build
 
 for file in *.sql 
 do
-    if [ "$1" == "delta" ]; then
+    if [ "$mode" == "delta" ]; then
 
         # we find difference from repo vs actual db
         object_schema=$(get_object_info_from_filename "$file" "schema")
@@ -64,11 +151,20 @@ do
         
         # first we get table info from db
         if [ "$object_type" == "Table" ]; then
-            result=$(get_table_info_from_db "$object_schema" "$object_name")
-            echo "$result"
+
+            echo "$object_name"
+            echo "---------------------------------------------------------"
+
+            # get table info from db
+            get_table_info_from_db_v3 "$object_schema" "$object_name"
+
             # then we get table info from repo
-            
+            get_table_info_from_file_v2 "$file"
+
             # find the difference
+            diff_result=$(find_difference_v2 $db_result $file_result)
+            echo "$diff_result"
+            echo "---------------------------------------------------------"
 
             # generate alter statement
         
@@ -86,7 +182,7 @@ do
 
         fi
 
-    elif [ "$1" == "full" ]; then
+    elif [ "$mode" == "full" ]; then
 
         # we force everything from repo
         if [[ $file == *"Table"* ]]; then
@@ -101,7 +197,7 @@ do
 done
 
 # we do this only for full build
-if [ "$1" == "full" ]; then
+if [ "$mode" == "full" ]; then
     for tmp in table.tmp view.tmp sp.tmp
     do
         cat $tmp >> database.sql
